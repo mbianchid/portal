8fbcd8da0589e157b6227fb2aa3cde8d4a741539
Miguel Bianchi Dias <mbianchid.12@gmail.com>
2024-05-23 01:35:06 -0300

    feat: dev cepSearch page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { FiSearch } from 'react-icons/fi';
+import './styles.css'
+
+import { api } from './services/api';
+
+interface CepData {
+  cep: string;
+  logradouro: string;
+  complemento: string;
+  bairro: string;
+  localidade: string;
+  uf: string;
+}
 
 function App() {
+
+  const [input, setInput] = useState('')
+  const [cep, setCep] = useState<CepData | null>(null)
+
+  async function handleSearch() {
+    if(input === '') {
+      alert('Digite um CEP para pesquisar!')
+      return
+    }
+  
+    if (!/^\d+$/.test(input)) {
+      alert('Digite apenas números no campo de busca!');
+      setInput('')
+      return;
+    }
+
+    try {
+      const response = await api.get(`${input}/json/`)
+      setCep(response.data)
+      setInput('')
+    } catch {
+      alert('CEP não encontrado')
+      setInput('')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscador de CEP</h1>
+
+      <div className="containerInput">
+        <input 
+          type="text" 
+          placeholder="Digite o seu CEP"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color='#fff'/>
+        </button>
+      </div>
+
+      {Object.keys(cep || {}).length > 0 && (
+        <main className='main'>
+          {cep ? (
+            <>
+              <h2>CEP: {cep.cep}</h2>
+              <span>Rua {cep.logradouro}</span>
+              <span>Complemento: {cep.complemento}</span>
+              <span>Bairro: {cep.bairro}</span>
+              <span>{cep.localidade} - {cep.uf}</span>
+            </>
+          ) : (
+              <h2>CEP não encontrado</h2>
+          )}
+        </main>       
+      )}
     </div>
   );
 }
